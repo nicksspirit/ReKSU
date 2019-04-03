a76d05acc591d0059189d7aa300894c88c2f7b00
Nick Spirit <nickspirit3+github@gmail.com>
2019-04-03 07:13:06 -0400

    Updated the Student Major generator

--- simulation/distributions.py
+++ simulation/distributions.py
@@ -4,13 +4,25 @@ from typing import List
 from pathlib import Path
 
 DATA_PATH = Path.cwd().parent / "distributions" / "data"
-majors_df = pd.read_csv(DATA_PATH / "major_prob.csv", index_col="F1SEQ1_MAJOR")
 
-MAJOR_CODES = majors_df.index.values
-MAJOR_PROBS = majors_df["Probability"].values
+majors_df = pd.read_csv(
+    DATA_PATH / "f1seq1_major_probs.csv", index_col="F1SEQ1_MAJOR")
+F1SEQ1_MAJOR_CODES = majors_df.index.values
+F1SEQ1_MAJOR_PROBS = majors_df["Probability"].values
 
 
-def gen_major(n_students: int) -> List[str]:
+def gen_major(n_students: int, semester: str = "s1seq1") -> List[str]:
+
+    if semester == "s1seq1":
+        majors = np.random.choice(
+            F1SEQ1_MAJOR_CODES,
+            size=n_students,
+            replace=True,
+            p=F1SEQ1_MAJOR_PROBS)
+
+    return majors
+
+
     majors = np.random.choice(
         MAJOR_CODES, size=n_students, replace=True, p=MAJOR_PROBS)
 
