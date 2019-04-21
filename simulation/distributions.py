aab8bc166aa80af503544ad25a4879faabee6eeb
Nick Spirit <nickspirit3+github@gmail.com>
2019-04-21 08:18:56 -0400

    Added Distribution of Majors from `S5SEQ2` to `F6SEQ2`

--- simulation/distributions.py
+++ simulation/distributions.py
@@ -75,6 +75,9 @@ class MajorSwitch:
         self.sem_major_df[("F5SEQ2_MAJOR", "S5SEQ2_MAJOR")] = pd.read_csv(
             DATA_PATH / "f5_s5_seq2_major_cprobs_2011.csv"
         )
+        self.sem_major_df[("S5SEQ2_MAJOR", "F6SEQ2_MAJOR")] = pd.read_csv(
+            DATA_PATH / "s5_f6_seq2_major_cprobs_2011.csv"
+        )
 
     def get_major(self, prev_sem: str, next_sem: str, curr_major: str) -> str:
         """Get major for next semester given current major
