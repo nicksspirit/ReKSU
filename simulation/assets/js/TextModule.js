ffe464b3efa86092783b93b1ade0d30ce990894f
Nick Muoh <nickspirit3+github@gmail.com>
2019-12-27 20:35:41 -0500

    Prettier formatting

--- simulation/assets/js/TextModule.js
+++ simulation/assets/js/TextModule.js
@@ -1,14 +1,14 @@
 var TextModule = function() {
-    let tag =  $("<div class='lead'></div>");
+  let tag = $("<div class='lead'></div>")
 
-    // Append text tag to #sidebar:
-    $("#sidebar").append(tag);
+  // Append text tag to #sidebar:
+  $('#sidebar').append(tag)
 
-    this.render = function(data) {
-        tag.html(data);
-    };
+  this.render = function(data) {
+    tag.html(data)
+  }
 
-    this.reset = function() {
-        tag.html("");
-    };
-};
+  this.reset = function() {
+    tag.html('')
+  }
+}
